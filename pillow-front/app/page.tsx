9f8ac6630949e0516d8f4d8c9c0e0de53b580921
nada-r <nadarzer@gmail.com>
2025-07-05 16:37:55 +0200

    add privy login

--- pillow-front/app/page.tsx
+++ pillow-front/app/page.tsx
@@ -1,97 +1,66 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { Suspense } from 'react'
-import Table from '@/components/table'
-import TablePlaceholder from '@/components/table-placeholder'
-import ExpandingArrow from '@/components/expanding-arrow'
+"use client";
 
-export const dynamic = 'force-dynamic'
+import Portal from "../components/portal";
+import { useLogin } from "@privy-io/react-auth";
+// import { PrivyClient } from "@privy-io/server-auth";
+// import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//   const cookieAuthToken = req.cookies["privy-token"];
+
+//   // If no cookie is found, skip any further checks
+//   if (!cookieAuthToken) return { props: {} };
+
+//   const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+//   const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET;
+//   const client = new PrivyClient(PRIVY_APP_ID!, PRIVY_APP_SECRET!);
+
+//   try {
+//     const claims = await client.verifyAuthToken(cookieAuthToken);
+//     // Use this result to pass props to a page for server rendering or to drive redirects!
+//     // ref https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
+//     console.log({ claims });
+
+//     return {
+//       props: {},
+//       redirect: { destination: "/dashboard", permanent: false },
+//     };
+//   } catch (error) {
+//     return { props: {} };
+//   }
+// };
+
+export default function LoginPage() {
+  const router = useRouter();
+  const { login } = useLogin({
+    onComplete: () => router.push("/vaultPage"),
+  });
+
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <Link
-        href="https://vercel.com/templates/next.js/postgres-prisma"
-        className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
-      >
-        <p>Deploy your own to Vercel</p>
-        <ExpandingArrow />
-      </Link>
-      <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Postgres on Vercel
-      </h1>
-      <Suspense fallback={<TablePlaceholder />}>
-        <Table />
-      </Suspense>
-      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
-        <Link
-          href="https://vercel.com/postgres"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Vercel Postgres
-        </Link>{' '}
-        demo with{' '}
-        <Link
-          href="https://prisma.io"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Prisma
-        </Link>{' '}
-        as the ORM. <br /> Built with{' '}
-        <Link
-          href="https://nextjs.org/docs"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Next.js App Router
-        </Link>
-        .
-      </p>
+    <>
+      <Head>
+        <title>Login · Privy</title>
+      </Head>
 
-      <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
-        <Link
-          href="https://postgres-starter.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Starter
-        </Link>
-        <Link
-          href="https://postgres-kysely.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Kysely
-        </Link>
-        <Link
-          href="https://postgres-drizzle.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Drizzle
-        </Link>
-      </div>
-
-      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
-        <Link href="https://vercel.com">
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width={100}
-            height={24}
-            priority
-          />
-        </Link>
-        <Link
-          href="https://github.com/vercel/examples/tree/main/storage/postgres-prisma"
-          className="flex items-center space-x-2"
-        >
-          <Image
-            src="/github.svg"
-            alt="GitHub Logo"
-            width={24}
-            height={24}
-            priority
-          />
-          <p className="font-light">Source</p>
-        </Link>
-      </div>
-    </main>
-  )
+      <main className="flex min-h-screen min-w-full">
+        <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
+          <div>
+            <div>
+              <Portal style={{ maxWidth: "100%", height: "auto" }} />
+            </div>
+            <div className="mt-6 flex justify-center text-center">
+              <button
+                className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg"
+                onClick={login}
+              >
+                Log in
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
