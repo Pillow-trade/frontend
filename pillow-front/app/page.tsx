fc254072a48806a0c366e22ba223f93b729b1b36
nada-r <nadarzer@gmail.com>
2025-07-05 17:12:05 +0200

    add privy login in a new vault component

--- pillow-front/app/page.tsx
+++ pillow-front/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Portal from "../components/portal";
 import { useLogin } from "@privy-io/react-auth";
 // import { PrivyClient } from "@privy-io/server-auth";
 // import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/navigation";
+import VaultPage from "../components/vaultPage";
 
 // export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 //   const cookieAuthToken = req.cookies["privy-token"];
@@ -35,7 +35,7 @@ import { useRouter } from "next/navigation";
 export default function LoginPage() {
   const router = useRouter();
   const { login } = useLogin({
-    onComplete: () => router.push("/vaultPage"),
+    // onComplete: () => router.push("/vaultPage"),
   });
 
   return (
@@ -47,16 +47,8 @@ export default function LoginPage() {
       <main className="flex min-h-screen min-w-full">
         <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
           <div>
-            <div>
-              <Portal style={{ maxWidth: "100%", height: "auto" }} />
-            </div>
             <div className="mt-6 flex justify-center text-center">
-              <button
-                className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg"
-                onClick={login}
-              >
-                Log in
-              </button>
+              <VaultPage />
             </div>
           </div>
         </div>
--- pillow-front/components/vaultPage.tsx
+++ pillow-front/components/vaultPage.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useLogin } from "@privy-io/react-auth";
 
 export default function CushionPage() {
+  const { login } = useLogin({
+    // onComplete: () => router.push("/vaultPage"),
+  });
   const [activeAction, setActiveAction] = useState<"deposit" | "withdraw">(
     "deposit"
   );
@@ -15,11 +19,12 @@ export default function CushionPage() {
           <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
             <div className="w-4 h-4 bg-[#0F1116] rounded-sm"></div>
           </div>
-          <h1 className="text-lg font-bold">CUSHION</h1>
+          <h1 className="text-lg font-bold">PILLOW</h1>
         </div>
         <Button
           className="bg-gradient-to-r from-[#2962FF] to-[#5C6BFF] hover:from-[#2962FF]/90 hover:to-[#5C6BFF]/90 rounded-xl h-10 px-6"
           aria-label="Connect wallet"
+          onClick={login}
         >
           Connect
         </Button>
