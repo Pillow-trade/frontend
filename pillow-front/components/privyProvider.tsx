efb4ab5f3a7f06107ce66d013f5b686bc11178be
nada-r <nadarzer@gmail.com>
2025-07-05 13:52:36 +0200

    add Privy ids

--- pillow-front/components/privyProvider.tsx
+++ pillow-front/components/privyProvider.tsx
@@ -5,8 +5,8 @@ import { PrivyProvider } from "@privy-io/react-auth";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
-      appId="your-privy-app-id"
-      clientId="your-app-client-id"
+      appId="cmcpcp4o100wcjp0nwneosqii"
+      clientId="client-WY6N5NwdFH9oABSeCjm9DpiWaAp572AnQzJNZndNo7WXa"
       config={{
         embeddedWallets: {
           ethereum: {
